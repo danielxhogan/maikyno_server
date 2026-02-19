6d6c2c11072f46fc3b6093dac865d412df16adce
danielxhogan <danielxhogan@gmail.com>
2026-02-19 11:01:25 -0800

    perform necessary formatting not already done by format filter

--- src/av/rendition.c
+++ src/av/rendition.c
@@ -3,6 +3,52 @@
 #include <libavutil/pixdesc.h>
 #include <libavutil/opt.h>
 
+int get_rendition_filter_string(char *flt_str, ProcessingContext *proc_ctx)
+{
+  StreamContext *stream_ctx = proc_ctx->stream_ctx_arr[0];
+  char rend0_flt_str[32], rend1_flt_str[256];
+
+  int rend0_convert_pix_fmt = 0;
+
+  if (proc_ctx->fmt_pix_fmt != proc_ctx->rend0_pix_fmt) {
+    rend0_convert_pix_fmt = 1;
+
+    snprintf(rend0_flt_str, 32, "format=%s",
+      av_get_pix_fmt_name(proc_ctx->rend0_pix_fmt));
+
+    printf("rend0_flt_str: %s\n", rend0_flt_str);
+  }
+
+  snprintf(rend1_flt_str, 256,
+    "libplacebo=w=%d:h=%d:downscaler=ewa_lanczos",
+    stream_ctx->rend1_enc_ctx->width, stream_ctx->rend1_enc_ctx->height);
+
+  if (proc_ctx->fmt_pix_fmt != proc_ctx->rend1_pix_fmt) {
+    strcat(rend1_flt_str, ":format=");
+    strcat(rend1_flt_str, av_get_pix_fmt_name(proc_ctx->rend1_pix_fmt));
+  }
+
+  if (proc_ctx->fmt_hdr) {
+    strcat(rend1_flt_str,
+      ":tonemapping=hable:color_primaries=bt709:color_trc=bt709:colorspace=bt709:chroma_location=left");
+  }
+
+  printf("rend1_flt_str: %s\n", rend1_flt_str);
+
+  if (rend0_convert_pix_fmt) {
+    snprintf(flt_str, 512,
+      "[in]split[split1][split2];[split1]%s[out1];[split2]%s[out2]",
+      rend0_flt_str, rend1_flt_str);
+  } else {
+    snprintf(flt_str, 512, "[in]split[out1][split2];[split2]%s[out2]",
+    rend1_flt_str);
+  }
+
+  printf("renditions flt_str: %s\n", flt_str);
+
+  return 0;
+}
+
 int buffersink_ctx_init(AVFilterContext **buffersink_ctx,
   AVFilterGraph *filter_graph, const char *pix_fmt)
 {
@@ -39,21 +85,14 @@ RenditionFilterContext *rendition_filter_context_init(
   char args[512], flt_str[512];
 
   AVCodecContext *dec_ctx = stream_ctx->dec_ctx;
-  AVCodecContext *enc_ctx1 = stream_ctx->rend0_enc_ctx;
-  AVCodecContext *enc_ctx2 = stream_ctx->rend1_enc_ctx;
   AVStream *in_stream = stream_ctx->in_stream;
 
-  const char *pix_fmt_str1 = av_get_pix_fmt_name(enc_ctx1->pix_fmt);
-  const char *pix_fmt_str2 = av_get_pix_fmt_name(enc_ctx2->pix_fmt);
+  const char *rend0_pix_fmt_str = av_get_pix_fmt_name(proc_ctx->rend0_pix_fmt);
+  const char *rend1_pix_fmt_str = av_get_pix_fmt_name(proc_ctx->rend1_pix_fmt);
 
-  if (proc_ctx->tonemap && proc_ctx->hdr) {
-    snprintf(flt_str, sizeof(flt_str),
-      "[in]split[out1][tmp];[tmp]zscale=t=linear:npl=100,format=gbrpf32le,zscale=p=bt709,tonemap=tonemap=hable,zscale=w=%d:h=%d:t=bt709:m=bt709:c=left,format=%s[out2]",
-      enc_ctx2->width, enc_ctx2->height, pix_fmt_str2);
-  } else {
-    snprintf(flt_str, sizeof(flt_str),
-      "[in]split[out1][tmp];[tmp]scale=w=%d:h=%d,format=%s[out2]",
-      enc_ctx2->width, enc_ctx2->height, pix_fmt_str2);
+  if ((ret = get_rendition_filter_string(flt_str, proc_ctx)) < 0) {
+    fprintf(stderr, "Failed to get filter string.\n");
+    goto end;
   }
 
   const AVFilter *buffersrc = avfilter_get_by_name("buffer");
@@ -95,7 +134,7 @@ RenditionFilterContext *rendition_filter_context_init(
 
   snprintf(args, sizeof(args),
     "video_size=%dx%d:pix_fmt=%d:time_base=%d/%d:pixel_aspect=%d/%d",
-    dec_ctx->width, dec_ctx->height, proc_ctx->formatted_pix_fmt,
+    dec_ctx->width, dec_ctx->height, proc_ctx->fmt_pix_fmt,
     in_stream->time_base.num, in_stream->time_base.den,
     dec_ctx->sample_aspect_ratio.num,
     dec_ctx->sample_aspect_ratio.den);
@@ -108,14 +147,14 @@ RenditionFilterContext *rendition_filter_context_init(
   }
 
   if ((ret = buffersink_ctx_init(&v_rend_ctx->buffersink_ctx1,
-    v_rend_ctx->filter_graph, pix_fmt_str1)) < 0)
+    v_rend_ctx->filter_graph, rend0_pix_fmt_str)) < 0)
   {
     fprintf(stderr, "Failed to initialize first buffer sink context.\n");
     goto end;
   }
 
   if ((ret = buffersink_ctx_init(&v_rend_ctx->buffersink_ctx2,
-    v_rend_ctx->filter_graph, pix_fmt_str2)) < 0)
+    v_rend_ctx->filter_graph, rend1_pix_fmt_str)) < 0)
   {
     fprintf(stderr, "Failed to initialize first buffer sink context.\n");
     goto end;
