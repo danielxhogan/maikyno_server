a360f161c02b3ae62c3ff4a9a4e7b6d13c72be70
danielxhogan <danielxhogan@gmail.com>
2026-02-25 09:23:15 -0800

    add release build

--- run.sh
+++ run.sh
@@ -1,2 +1,5 @@
-PKG_CONFIG_PATH=/home/hugexjackedman/ffmpeg/ffmpeg_build/lib/pkgconfig:/usr/lib/x86_64-linux-gnu/pkgconfig cargo run
+# PKG_CONFIG_PATH=/home/hugexjackedman/ffmpeg/ffmpeg_build/lib/pkgconfig:/usr/lib/x86_64-linux-gnu/pkgconfig cargo run
+
+PKG_CONFIG_PATH=/home/hugexjackedman/ffmpeg/ffmpeg_build/lib/pkgconfig:/usr/lib/x86_64-linux-gnu/pkgconfig cargo run --release
+
 # PKG_CONFIG_PATH=/home/hugexjackedman/ffmpeg/ffmpeg_build/lib/pkgconfig:/usr/lib/x86_64-linux-gnu/pkgconfig cargo build
